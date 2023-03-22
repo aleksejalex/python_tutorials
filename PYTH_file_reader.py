91a397543e31127dadcf66e7b5660e83a6f243c3
aleksejalex <29196199+aleksejalex@users.noreply.github.com>
2023-03-22 11:31:16 +0100

    minors

---
+++ PYTH_file_reader.py
@@ -0,0 +1,11 @@
+"""
+
+The goal is to write a code (ideally a function) that reads a text file containing numbers and returns the arithmetic
+mean of the numbers in the file. The file is assumed to contain one number per line. The function should be able to
+handle both integers and floating point numbers."""
+
+
+
+
+
+
